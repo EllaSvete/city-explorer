e482d9bd02bcddbb4692b3803121967c4f732a5a
Ella Svete <ella.svete@gmail.com>
2022-03-21 16:57:48 -0700

    steps 1&2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,51 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import './App.css';
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cityData: {},
+    };
+  }
+
+  handleCity = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  };
+  
+  getCityData = async (e) => {
+    e.preventDefault();
+    // get API data
+    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${this.state.city}&format=json`);
+    this.setState({
+      cityData: cityData.data[0],
+    });
+    console.log(this.state.cityData);
+  }
+  
   render() {
     return (
       <>
       <Header/>
       <Main/>
-      <Footer/>
-
+    <main>
+      <form onSubmit={this.getCityData}>
+        <label>Pick a City
+          <input type="text" onInput={this.handleCity} name="city"/>
+        </label>
+        <button type="submit">Explore!</button>
+      </form>
+    </main>
+    <p>lon: {this.state.cityData.lon}</p>
+    <p>lat:{this.state.cityData.lat}</p>
+    <p>name:{this.state.cityData.display_name}</p>
+    <Footer/>
       </>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,13 @@
 import React from 'react';
 
 class Main extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      city: ''
-    };
-  }
 
-  handleCity = (e) => {
-    this.setState({
-      city: e.target.value
-    });
-  };
+
 
   render() {
     console.log(this.state);
     return (
-    <main>
-      <form>
-        <label>Pick a City
-          <input type="text" onInput={this.handleCity} name="city"/>
-        </label>
-        <button type="submit">Find City</button>
-      </form>
-    </main>
+    <main></main>
     )
   }
 };
